d05219b7aa730a9e2d078f9d594106d08df26cce
luis eduardo villa ojeda <112597598+lvilla8@users.noreply.github.com>
2023-09-05 11:12:48 -0500

    base

--- Servidor/index.js
+++ Servidor/index.js
@@ -1,8 +1,17 @@
-const http = require('http')
-const port =3000
+// const http = require('http')
+// const port =3000
 
-const server = http.createServer((req,res)=> {
-    res.end('Respuesta')
+// const server = http.createServer((req,res)=> {
+//     res.end('Respuesta')
+// })
+
+// server.listen(port, ()=> console.log('Servidor funciona'))
+
+const express = require ('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res)=>{
+    res.send("Hola mundo")
 })
-
-server.listen(port, ()=> console.log('Servidor funciona'))
+app.listen(port)
